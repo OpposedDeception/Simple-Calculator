b52745a2be87f2dd932c881e7ebcab0f3672e5e1
KotenAngered <113610915+KotenAngered@users.noreply.github.com>
2022-11-27 18:46:33 +0100

    Rewritten

--- enhancedcalculator.py
+++ enhancedcalculator.py
@@ -1,69 +1,74 @@
-import math
+from colorama import Fore, Back, init
+import sys
 
-def add(x, y):
-    return x + y
-
-def minus(x, y):
-    return x - y
-
-def multiply(x, y):
-    return x * y
-
-def divide(x, y):
-    return x / y
-
-def double(x, y):
-    return x ** y
-
-def percentage(x, y):
-    return x % y
-
-def math_sqrt(x):
-    return math.sqrt(x)
+class calculator:
+    def __init__(self, x, y):
+        self.x = int(input("Write first number: "))
+        self.y = int(input("Write second number: "))
     
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-print("5.Double")
-print("6.Percentage")
-print("7.Square root")
-
-while True:
-    choice = input("Enter choice(1/2/3/4/5/6/7): ")
-
-    if choice in ('1', '2', '3', '4', '5', '6'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number, not necessary if it is a square root: "))
-
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-
-        elif choice == '2':
-            print(num1, "-", num2, "=", minus(num1, num2))
-
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-            
-        elif choice == '5':
-            print(num1, "**", num2, "=", double(num1, num2))
-            
-        elif choice == '6':
-            print(num1, "%", num2, "=", percentage(num1, num2))
-                    
-        elif choice == '7':
-            print(math.sqrt(num1), "=", math_sqrt(num1))
-        
-        
-        new_calculation = input("Do we continue m8? (yep/nah): ")
-        if new_calculation == "nah":
-          break
-        
+    def multiply(self):
+        return self.x * self.y
+                     
+    def substract(self):
+        return self.x - self.y
+                     
+    def divide(self):
+        return self.x / self.y
      
-    else:
-        print("Bad input bro")
+    def add(self):
+        return self.x + self.y
     
+    def modulus(self):
+        return self.x % self.y
+                       
+    def double(self):
+        return self.x ** self.y
+    
+init(convert=True)
+
+print(Back.GREEN + Fore.RED + 'Welcome to calculator!')
+print('1. Add')
+print('2. Multiply')
+print('3. Substract')
+print('4. Divide')
+print('5. Modulus')
+print('6. Double')
+
+choice = input("Enter choice: ")
+    
+user_input = calculator(0, 0)
+
+
+
+    
+if choice in('1', '2', '3', '4', '5', '6'):      
+    if choice == '1':
+        add_num = user_input
+        print(add_num.add())
+        input()
+        sys.exit()
+    if choice == '2':
+        multiply_num = user_input
+        print(multiply_num.multiply())
+        input()
+        sys.exit()
+    if choice == '3':
+        substract_num = user_input
+        print(substract_num.substract())
+        input()
+        sys.exit()
+    if choice == '4':
+        divide_num = user_input
+        print(divide_num.divide())
+        input()
+        sys.exit()
+    if choice == '5':
+        modulus_num = user_input
+        print(modulus_num.modulus())
+        input()
+        sys.exit()
+    if choice == '6':
+        double_num = user_input
+        print(double_num.double())
+        input()
+        sys.exit()
